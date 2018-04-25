df742204f3b10de0202a51e3b5f7c364bf545ca0
semihcavdarr <scavdar@gtu.edu.tr>
2018-04-25 21:28:02 +0300

    Update Hangman.hpp

--- Hangman_Example/lib/Hangman.hpp
+++ Hangman_Example/lib/Hangman.hpp
@@ -1,39 +1,21 @@
-#ifndef DEF_GAME
-#define DEF_GAME
+#ifndef DEF_HANGMAN
+#define DEF_HANGMAN
 
 #include <iostream>
 #include <string>
-#include "SecretPhrase.hpp"
-#include "Hangman.hpp"
 
-class Game {
+class Hangman {
   public:
-    Game();
-    ~Game();
-    void play();
-    void updateBoard();
-    void parsePlayerGuess(char guess);
-    void displayResults();
-    void setPlayerSecretPhrase();
-
-    char getPlayerGuess();
-
-    bool isGameOver();
-    bool wantToPlayAgain();
-    std::string generateRandomWord();
-    static std::string getPlayerAnswer(std::string prompt_msg, std::string error_msg);
+    Hangman();
+    ~Hangman();
+    int getStatus();
+    void updateStatus(int newValue);
+    std::string draw(int status);
+    void resetStatus();
 
   private:
-    SecretPhrase *m_secretPhrase;
-    Hangman *m_hangman;
-    std::string  secretWord;
-    int playerFirstScore;
-    int playerSecondScore;
-    bool turn;
-    void displayWinner();
-    void displayLoser();
-    void writeFile();
-    char lastChar;
+    int m_status;
+
 };
 
 #endif
